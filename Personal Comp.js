080c5cfb0fcb6c21902b88d7846e802595496343
alucas5420 <112018521+alucas5420@users.noreply.github.com>
2022-08-31 18:51:46 -0400

    Add files via upload

--- Personal Comp.js
+++ Personal Comp.js
@@ -1,20 +1,24 @@
+// Setting up the canvas to draw the images on
+
 function setup() {
     createCanvas(400, 300);
 }
 
+// Draws the images
+
 function draw() {
     background(200, 0, 200);
-    line(20, 50, 420, 110);
+    line(20, 50, 420, 110); // Draws a line
 
-    rectMode(CENTER);
-    rect(200, 150, 150, 150);
+    rectMode(CENTER); // Centers the rectangle
+    rect(200, 150, 150, 150); // Draws a rectangle
 
-    fill(255, 0, 0);
-    ellipse(150, 100, 100, 100);
+    fill(255, 0, 0); // Fills in the color
+    ellipse(150, 100, 100, 100); // Makes a circle
 
-    fill(0, 255, 0);
-    ellipse(120, 80, 80, 80);
+    fill(0, 255, 0); // Fills in the color
+    ellipse(120, 80, 80, 80); // Makes a circle
 
-    fill(0, 0, 255);
-    ellipse(100, 60, 60, 60);
+    fill(0, 0, 255); // Fills in the color
+    ellipse(100, 60, 60, 60); // Makes a circle
 }
